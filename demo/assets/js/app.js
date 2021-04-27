625122ec3859451b06dbec390c49bce5c535dc53
Thomas Jarrand <thomas.jarrand@elao.com>
2021-04-27 17:42:10 +0200

    Symfony + Stenope based demo

---
+++ demo/assets/js/app.js
@@ -0,0 +1,11 @@
+import { Collapsible, Drop, Tree, MobileSidebar } from '@elao/admin';
+
+window.addEventListener('load', function onLoad() {
+    // Behaviors
+    Collapsible.init();
+    Drop.init();
+    Tree.init();
+
+    // Mobile
+    MobileSidebar.init();
+});
---
+++ demo/src/Controller/DemoController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DemoController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function home(): Response
+    {
+        return $this->render('demo/home.html.twig', []);
+    }
+}
