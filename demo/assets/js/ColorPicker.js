0085223f38ac7edfc429cd6babf23ed193e3245c
Thomas Jarrand <thomas.jarrand@elao.com>
2021-05-17 09:23:06 +0200

    Fixed hover state

--- demo/assets/js/ColorPicker.js
+++ demo/assets/js/ColorPicker.js
@@ -1,8 +1,21 @@
 export default class ColorPicker {
     constructor(element) {
         this.element = element;
+        this.parser = /^#(\w{2})(\w{2})(\w{2})$/i;
 
-        this.element.addEventListener('change', this.onChange.bind(this));
+        this.onChange = this.onChange.bind(this);
+
+        this.element.addEventListener('change', this.onChange);
+
+        this.load();
+    }
+
+    load() {
+        const color = localStorage.getItem('color');
+
+        if (color) {
+            this.setValue(color);
+        }
     }
 
     onChange() {
@@ -10,6 +23,16 @@ export default class ColorPicker {
     }
 
     setValue(value) {
+        localStorage.setItem('color', value);
+        this.element.value = value;
         document.body.style.setProperty('--primary', value);
+        document.body.style.setProperty('--primary--dark', this.modify(value, v => v - 25));
+    }
+
+    modify(value, change) {
+        const colors = this.parser.exec(value.trim()).slice(1)
+            .map(value => Math.min(Math.max(change(parseInt(value, 16)), 0), 255).toString(16).padStart(2, '0'));
+
+        return `#${colors.join('')}`;
     }
 }
